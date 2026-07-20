9abfc90c914f83d1a82e630e1b28c7459d8ad4e4
AndreNicolay <132912126+AndreNicolay@users.noreply.github.com>
2026-07-20 19:50:56 -0300

    Atividade com algoritmos

---
+++ algoritmos/script.js
@@ -0,0 +1,27 @@
+// 1
+const nome = "André"
+const sobrenome = "Nicolay"
+const nomeCompleto = nome + sobrenome
+
+console.log("Olá meu nome completo é: ", nomeCompleto)
+
+// 2
+const ano = 2007
+const atual = 2026
+const idade = atual - ano
+
+console.log("Minha idade é:", idade)
+
+// 3
+const numero = 48
+if (numero % 2 === 0) {
+    console.log("Este número é par")
+} else {
+    console.log("Este número é impar")
+}
+
+// 4
+const valor = 380
+const valorDesconto = valor - (valor * 0.10)
+
+console.log("O valor deste produto com desconto é:", valorDesconto)
