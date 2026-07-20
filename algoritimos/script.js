aecdea5a4a00c873e8af26544d471c916f474be2
AndreNicolay <132912126+AndreNicolay@users.noreply.github.com>
2026-07-20 19:28:37 -0300

    algoritimos

---
+++ algoritimos/script.js
@@ -0,0 +1,24 @@
+console.log("rodou")
+
+//operações matemáticas
+// + - * /
+const numero1 = 10
+const numero2 = 5
+//decimais representados pelo "."
+const resultado = numero1 / numero2
+console.log(resultado, 'resultado')
+
+const nome = "André"
+const sobrenome = "Nicolay"
+const saudação = "Olá meu nome completo é: "
+const nomeCompleto = nome + sobrenome
+//templete string
+const templeteString = `Olá meu nome completo é: ${nome} ${sobrenome}`
+
+console.log(saudação, nomeCompleto)
+console.log(templeteString)
+
+//operadores % "mod"
+const numero = 2
+console.log(numero%2)
+
